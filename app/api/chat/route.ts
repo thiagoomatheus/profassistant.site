d4a1a406ef9120a09d0c9bd81378fe3be700bf97
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-11-11 10:38:11 -0300

    1

---
+++ app/api/chat/route.ts
@@ -0,0 +1,31 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+ 
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+ 
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+ 
+export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
+  const { messages } = await req.json();
+ 
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages,
+    max_tokens: 600,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  });
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
