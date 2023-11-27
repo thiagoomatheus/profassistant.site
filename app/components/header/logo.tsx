7d4c6d3344ee5fa17f39bcc9ae529d41f7638e9d
Thiago Oliveira Matheus <117959734+thiagoomatheus@users.noreply.github.com>
2023-11-27 20:00:58 -0300

    feat: import images for home and add logo

--- app/components/header/logo.tsx
+++ app/components/header/logo.tsx
@@ -1,9 +1,15 @@
+import Logotyope from "../../../public/images/Logo.png"
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Logo () {
     return (
-        <Link href={"/"} className="w-24 h-15 font-bold text-white text-3xl border-b-8">
-            Question! Generator
+        <Link href={"/"}>
+            <Image 
+                src={Logotyope} 
+                alt="Question Generator" 
+                width={96}
+            />
         </Link>
     )
 }
