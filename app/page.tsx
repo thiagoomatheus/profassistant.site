6cf789db6ecf17a4416fe574ce6f1d68b6f4bba0
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-01-22 09:29:53 -0300

    added line breaks for better viewing

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,12 @@ import Third from "@/public/images/Passo 3.gif"
 import { plans } from "./(plans)/plans"
 import CardPrice from "./components/layout/cardPrice"
 import Button from "./components/layout/button"
+
 export default function Home() {
+
   return (
     <main key={"main"} className="mx-auto w-full p-0 flex flex-col gap-14 md:gap-20 xl:gap-28">
+
       <section key={"apresentation"} className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 self-center max-w-[1440px] self gap-5">
         <ContainerText key={"text_apresentation"} aditionalCSS="lg:text-left lg:max-w-[400px] xl:max-w-lg">
           <h1 className="gradient p-2">Crie textos e questões para seus alunos em segundos</h1>
@@ -26,11 +29,14 @@ export default function Home() {
           <Image priority key={"image_apresentation"} src={Educator} alt="Professor aplicando prova para alunos" />
         </div>
       </section>
+
       <section key={"product"} id="produto" className="flex flex-col gap-7 md:gap-10 xl:gap-20 items-center">
+
         <ContainerText key={"text_product"} aditionalCSS="h-72 xl:h-[400px] md:max-w-sm lg:max-w-2xl reveal-text">
           <h1>A ferramenta que irá te surpreender!</h1>
           <p>Nós sabemos o quanto você professor tem grandes responsabilidades. E junto a isso, surge várias tarefas: preparar aulas, manter cronogramas educacionais atualizados, dar suporte aos alunos, entre muitas outras. Pensando em te ajudar, criamos essa ferramenta incrível que se tornará seu braço direito. Veja o que nossa ferramenta pode te proporcionar:</p>
         </ContainerText>
+
         <div key={"cards_product"} className="flex flex-col md:flex-row gap-5 lg:gap-10 reveal-card-left">
           <Card key={"card_time"} title="Tempo" img={Time} aditionalCSS="border-[10px] lg:border-[15px] border-orange-2">
             <p>Gere questões, textos, frases ou expressões matemáticas em alguns segundos e tenha mais tempo para você.</p>
@@ -42,15 +48,22 @@ export default function Home() {
             <p>Aqui você pode copiar a informação de forma fácil e montar a sua prova ou atividade.</p>
           </Card>
         </div>
+
       </section>
+
       <section key={"how_to_use"} className="flex flex-col gap-5 md:gap-10">
+
         <h2 className="text-center">Veja como usar:</h2>
         <SectionTwoColumns key={"first"} title="1° - Selecione as informações dos alunos e o que deseja gerar" text="Coloque a série, a idade média dos alunos e selecione o que você deseja gerar: questão, texto, frase ou expressão matemática" img={First} borderColor="orange"/>
         <SectionTwoColumns key={"second"} title="2° - Forneça mais informações" text="Aqui você vai nos dar mais informações, por exemplo, talvez você precise fornecer a matéria, a quantidade ou o assunto." img={Second} borderColor="blue" reverse={true} />
         <SectionTwoColumns key={"third"} title="3° - Clique em “Gerar”" text="Agora é so aguardar alguns segundos e o que você gerou vai aparecer na tela" img={Third} borderColor="orange" />
+
       </section>
+
       <section key={"plans"} id="planos" className="flex flex-col gap-7 md:gap-20 xl:gap-28 items-center">
+
         <h1>Preços e planos:</h1>
+
         <div key={"cards_plans"} className="flex flex-col justify-center md:grid md:grid-cols-3 gap-5 lg:gap-10">
           {plans.map(plan => (
             <CardPrice key={`card_${plan.id}`} plan={plan} title={plan.name} price={plan.price} className={plan.name === "Pro" ? "animate-bounce-slow hover:animate-none duration-500" : ""} >
@@ -58,14 +71,21 @@ export default function Home() {
             </CardPrice>
           ))}
         </div>
+
       </section>
+
       <section key={"about"} id="sobre" className="flex flex-col justify-center items-center gap-5 md:gap-10 text-center">
+
         <ContainerText key={"text_about"} aditionalCSS="gap-5 md:gap-7 md:max-w-xl xl:max-w-3xl">
+
           <h1 className="self-center">Sobre nós:</h1>
           <p>O projeto {process.env.NEXT_PUBLIC_PROJECT_NAME} teve seu início em janeiro de 2024 nascendo da ideia de facilitar o trabalho dos profissionais da educação. Seu criador possui professores no círculo familiar e percebia a dificuldade deles em achar questões, textos, frases ou expressões matemáticas para suas provas e atividades e, que de uma maneira facilitada, pudessem organizá-las e remodelá-las.</p>
           <p>Nosso obetivo é ajudar você, professor, a ter essa faceta de seu precioso trabalho simplificada e agilizada, podendo proporcionar a você mais tempo. Nesse projeto, você tem parte crucial, assim, nos esforçamos em proporcionar a ferramenta mais completa para você!</p>
+
         </ContainerText>
+
         <h2>Veja o que dizem sobre nós:</h2>
+
         <div className="flex flex-col md:flex-row gap-5 lg:gap-10">
           <Card key={"card_person1"} title="Raquel" aditionalCSS="border-4 border-blue">
             <p>Perdia muito tempo procurando questões interessantes, mas agora consigo criar questões de forma fácil e rápida</p>
@@ -74,6 +94,7 @@ export default function Home() {
             <p>Achei a ferramenta muito simples e fácil. Agora consigo preparar aulas mais rápido e ainda sobre tempo pra mim</p>
           </Card>
         </div>
+        
       </section>
     </main>
   )
