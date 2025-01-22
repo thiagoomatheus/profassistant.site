c9829d2df90c31291cc5b1da51bd31f8bdb3fb1b
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-01-21 22:31:55 -0300

    style adjustments

--- app/components/layout/sectionWithBorder.tsx
+++ app/components/layout/sectionWithBorder.tsx
@@ -3,7 +3,7 @@ export default function SectionWithBorder( { borderColor, children }: {
     children: React.ReactNode
 } ) {
     return (
-        <div className={`flex flex-col justify-start items-center gap-2 p-2 rounded-xl ${borderColor ? `border ${borderColor}` : ""} `}>
+        <div className={`flex flex-col justify-start items-center gap-2 md:gap-5 p-2 rounded-xl w-full ${borderColor ? `border ${borderColor}` : ""} `}>
             {children}
         </div>
     )
